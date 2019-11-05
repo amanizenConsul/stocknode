5a73d994d5a3ad1fb77187ab978ce20b77920012
Adrian Green <adrianamanigreen@gmail.com>
2019-11-05 14:45:50 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const exphbs  = require('express-handlebars')
+
+const PORT = process.env.PORT || 5000
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "Hi, this is other stuff!"
+app.get('/', function (req, res) {
+    res.render('home', {stuff:otherstuff});
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT))
